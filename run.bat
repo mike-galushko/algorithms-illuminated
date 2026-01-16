ff437a11a450da496f216b62d6232f83cca43254
Mike Galushko <mick.galushko@gmail.com>
2026-01-16 14:24:45 +0300

    Improve run script

--- run.bat
+++ run.bat
@@ -1,8 +1,14 @@
-@echo off 
-set "fileName=1.1-integer-multiply-school.cpp"
+@echo off
+
+@REM Select example
+set "fileName=1.1-integer-multiply-school"
+
+@REM Compile example
 set "outputPath=./output"
 set "inputPath=./Part1-basics"
 if not exist "%outputPath%/" mkdir "%outputPath"
+clang++ "%inputPath%/%fileName%.cpp" -o "%outputPath%/%fileName%.exe" 
 
+@REM Run example
 @echo on
-clang++ "%inputPath%/%fileName%" -o "%outputPath%/%fileName%" 
+"%outputPath%/%fileName%.exe"
