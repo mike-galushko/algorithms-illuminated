45899daecdfb89e26bc084a99eae0d89f09911c8
Mike Galushko <mick.galushko@gmail.com>
2026-01-21 00:56:35 +0300

    School method. Prepare input numbers

--- Part1-basics/1.1-integer-multiply-school.cpp
+++ Part1-basics/1.1-integer-multiply-school.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int SchoolAlgorithm(int lhs, int rhs);
+std::vector<char> CreateInput(std::string text);
+int SchoolAlgorithm(std::vector<char> &lhs, std::vector<char> &rhs);
 
 // Школьный алгоритм умножения двух чисел
 int main()
 {
-    int lhs, rhs = 0;
+    // Read input from console
+    std::string lhsText;
+    std::string rhsText;
 
     std::cout << "Multiply to numbers. The school algorithm.\n";
     std::cout << "Enter first number: ";
-    std::cin >> lhs;
+    std::cin >> lhsText;
     std::cout << "Enter second number: ";
-    std::cin >> rhs;
+    std::cin >> rhsText;
 
+    // Run algorithm
+    std::vector<char> lhs = CreateInput(lhsText);
+    std::vector<char> rhs = CreateInput(rhsText);
     int result = SchoolAlgorithm(lhs, rhs);
-    std::cout << lhs << " * " << rhs << " = " << result;
+
+    // Write output to console
+    std::cout << lhsText << " * " << rhsText << " = " << result << "\n";
 }
 
-int SchoolAlgorithm(int lhs, int rhs)
+int SchoolAlgorithm(std::vector<char> &lhs, std::vector<char> &rhs)
 {
     return 10;
+}
+
+// Convert input text to numbers array
+std::vector<char> CreateInput(std::string text)
+{
+    std::vector<char> result(text.length());
+    for (int i = 0; i < text.length(); ++i)
+    {
+        char val = text[i];
+        result[i] = val - '0';
+    }
+    return result;
 }
