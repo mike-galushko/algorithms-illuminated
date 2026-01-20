409493709a6ca953f07116a104441383907eb358
Mike Galushko <mick.galushko@gmail.com>
2026-01-21 00:13:10 +0300

    Read input for school algorithm

--- Part1-basics/1.1-integer-multiply-school.cpp
+++ Part1-basics/1.1-integer-multiply-school.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+int SchoolAlgorithm(int lhs, int rhs);
+
+// Школьный алгоритм умножения двух чисел
 int main()
 {
-    std::cout << "Hallo world!";
+    int lhs, rhs = 0;
+
+    std::cout << "Multiply to numbers. The school algorithm.\n";
+    std::cout << "Enter first number: ";
+    std::cin >> lhs;
+    std::cout << "Enter second number: ";
+    std::cin >> rhs;
+
+    int result = SchoolAlgorithm(lhs, rhs);
+    std::cout << lhs << " * " << rhs << " = " << result;
+}
+
+int SchoolAlgorithm(int lhs, int rhs)
+{
+    return 10;
 }
